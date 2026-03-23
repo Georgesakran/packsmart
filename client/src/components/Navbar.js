ac151b58956ef960a6657ee848bee0aed0259ace
Georgesakran <gsakran14@gmail.com>
2026-03-23 15:37:20 +0200

    home page updated

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="navbar-inner">
-        <h2 className="navbar-logo">Luggix 3D</h2>
+        <h2 className="navbar-logo">🧳 PackSmart</h2>
         <span className="navbar-tagline">Smart suitcase planning for travelers</span>
       </div>
     </nav>
