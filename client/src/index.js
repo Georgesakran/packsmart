2ac26c5f56d23333345aaa37d3514521c48d04e5
Georgesakran <gsakran14@gmail.com>
2026-03-27 16:10:08 +0300

    upd app.js and index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./styles/App.css";
 import App from "./App";
 import { AuthProvider } from "./contexts/AuthContext";
-import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
 );
