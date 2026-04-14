8a70616be3fdeae8c7d8010c3e78f3ecd67bd25c
Georgesakran <gsakran14@gmail.com>
2026-04-14 12:30:27 +0300

    upd tripSimulation Routes

--- server/routes/tripSimulationRoutes.js
+++ server/routes/tripSimulationRoutes.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const { protect } = require("../middleware/authMiddleware");
 
-const { authenticateToken } = require("../middleware/authMiddleware");
-const {
-  getTripSimulationScene,
-} = require("../controllers/tripSimulationController");
+const {getTripSimulationScene,} = require("../controllers/tripSimulationController");
 
-router.get("/trips/:id/simulation", authenticateToken, getTripSimulationScene);
+router.get("/trips/:id/simulation", protect, getTripSimulationScene);
 
 module.exports = router;
